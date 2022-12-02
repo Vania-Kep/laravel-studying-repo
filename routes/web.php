25c230d0c39d83a796d4d53d79a50a21b07bd8ac
I.Kapitan <vkapitan67@gmail.com>
2022-12-02 16:18:34 +0200

    LARAVEL-0002:ADD: Lab 2 (Commit2): Add Auth Middleware. Add link to users list in mashboard menu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/users');
 });
+Route::resource('users', UserController::class)->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
